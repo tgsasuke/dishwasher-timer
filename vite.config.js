15d35cda3540c2a38aaf6f10e82acb93dbfadc97
lisa_huang <lisa_huang@avalue.com.tw>
2026-06-10 11:44:20 +0800

    modified base url

--- vite.config.js
+++ vite.config.js
@@ -12,7 +12,7 @@ export default defineConfig({
     vueDevTools(),
     tailwindcss(),
   ],
-  base: '/dishwasher-timmer/',
+  base: '/dishwasher-timer/',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
